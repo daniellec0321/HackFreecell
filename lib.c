db4afc32c6fe8ae6a5d4fb2ab26775a014d8f3c2
Danielle Croft <daniellec0321@gmail.com>
2024-04-21 23:41:57 -0400

    Trying to create shared library

---
+++ lib.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "lib.h"
+
+int test_func(char *erm) {
+    printf("%s\n", erm);
+    return 0;
+}
---
+++ read_memory.py
@@ -0,0 +1,4 @@
+from ctypes import *
+import os
+
+lib = CDLL("./liblib.dll")

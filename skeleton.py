06bb68928f0249d2d03c745ecdfd08d11091ca17
Danielle Croft <daniellec0321@gmail.com>
2024-04-25 14:49:15 -0400

    Fix naming conventions

--- skeleton.py
+++ skeleton.py
@@ -5,30 +5,30 @@ COLUMNSIZE = 19
 NUMCOLUMNS = 8
 HFLIB = ct.CDLL("lib/hackFreecell.dll")
 
-def get_tableau(board: list[int]) -> bool:
+def get_freecells(board: list[int]) -> bool:
+    'Edits the given board to fill it with the freecells. The return value is 0 on success, -1 on failure'
+    temp = (ct.c_ubyte * 4)()
+    result = HFLIB.get_freecells(temp)
+    if (result == -1):
+        return False
+    for i in range(4):
+        board[i] = temp[i]
+    return True
+
+def get_foundations(board: list[int]) -> bool:
+    'Edits the given board to fill it with the foundations. The return value is 0 on success, -1 on failure'
+    temp = (ct.c_ubyte * 4)()
+    result = HFLIB.get_foundations(temp)
+    if (result == -1):
+        return False
+    for i in range(4):
+        board[i] = temp[i]
+    return True
+
+def get_tableau(board: list[list[int]]) -> bool:
     'Edits the given board to fill it with the tableau. The return value is 0 on success, -1 on failure'
-    temp = (ct.c_ubyte * 4)()
-    result = HFLIB.get_tableau(temp)
-    if (result == -1):
-        return False
-    for i in range(4):
-        board[i] = temp[i]
-    return True
-
-def get_stack(board: list[int]) -> bool:
-    'Edits the given board to fill it with the stack. The return value is 0 on success, -1 on failure'
-    temp = (ct.c_ubyte * 4)()
-    result = HFLIB.get_stack(temp)
-    if (result == -1):
-        return False
-    for i in range(4):
-        board[i] = temp[i]
-    return True
-
-def get_columns(board: list[list[int]]) -> bool:
-    'Edits the given board to fill it with the columns. The return value is 0 on success, -1 on failure'
     temp = (ct.c_ubyte * (NUMCOLUMNS*COLUMNSIZE))()
-    result = HFLIB.get_columns(temp)
+    result = HFLIB.get_tableau(temp)
     if (result == -1):
         return False
     for i in range(NUMCOLUMNS):
@@ -37,32 +37,31 @@ def get_columns(board: list[list[int]]) -> bool:
     return True
 
 def main():
-    # Get the tableau
-    tableau = [0, 0, 0, 0]
-    if (not get_tableau(tableau)):
-        print('Failed getting the tableau')
+
+    freecells = [0, 0, 0, 0]
+    if (not get_freecells(freecells)):
+        print('Failed getting the freecells')
         return 1
-    stack = [0, 0, 0, 0]
-    if (not get_stack(stack)):
-        print('Failed getting the stack')
+    foundations = [0, 0, 0, 0]
+    if (not get_foundations(foundations)):
+        print('Failed getting the foundations')
         return 1
-    
-    # Have to make this 'columns' list weirdly because of how python copies lists :/
-    columns = list()
+
+    tableau = list()
     for i in range(NUMCOLUMNS):
         l = list()
         for j in range(COLUMNSIZE):
             l.append(0)
-        columns.append(l.copy())
-    if (not get_columns(columns)):
-        print('Failed getting the columns')
+        tableau.append(l.copy())
+    if (not get_tableau(tableau)):
+        print('Failed getting the tableau')
         return 1
-    
-    print(f'The tableau is {tableau}')
-    print(f'The stack is {stack}')
-    print(f'The columns are')
-    for c in columns:
-        print(c)
+
+    print(f'The freecells are {list(map(lambda l: hex(l), freecells))}')
+    print(f'The foundations are {list(map(lambda l: hex(l), foundations))}')
+    print(f'The tableau is')
+    for c in tableau:
+        print(list(map(lambda l: hex(l), c)))
 
     return 0
 

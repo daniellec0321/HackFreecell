3ada1411f3201f2044e6015eae59c0f94bc67526
Danielle Croft <daniellec0321@gmail.com>
2024-04-30 15:39:21 -0400

    more GUI stuff

--- my-gui.py
+++ my-gui.py
@@ -1,9 +1,11 @@
-import tkinter as tk
 import sys
+import subprocess
+from typing import Optional
+import tkinter as tk
 from readProgram import readProgram
-from solve import Solve
+# from solve import Solve
 rp = readProgram()
-erm = Solve()
+# erm = Solve()
 
 class Solver:
 
@@ -12,6 +14,7 @@ class Solver:
         self.states = list()
         self.curr_state = tuple()
         self.curr_move = tuple()
+        self.mode = 'Automatic'
         self.lastMove = -1
         self.totalMoves = -1
         self.root = None
@@ -19,44 +22,185 @@ class Solver:
 
     def inc_move(self):
         if self.lastMove < self.totalMoves:
+            self.mode = 'Manual'
             self.lastMove += 1
 
     def dec_move(self):
         if self.lastMove > 0:
+            self.mode = 'Manual'
             self.lastMove -= 1
+
+    def convertBoard(self, board: list[list[int]]) -> list[list[str]]:
+        'New function, check this'
+        ret = list()
+        for row in board:
+            to_add = list()
+            for card in row:
+                num = str(int(card / 4) + 1)
+                if num == '1':
+                    num = 'A'
+                elif num == '11':
+                    num = 'J'
+                elif num == '12':
+                    num = 'Q'
+                elif num == '13':
+                    num = 'K'
+                suit = card % 4
+                if suit == 0:
+                    suit = 'C'
+                elif suit == 1:
+                    suit = 'D'
+                elif suit == 2:
+                    suit = 'H'
+                else:
+                    suit = 'S'
+                new_card = num + suit
+                to_add.append(new_card)
+            ret.append(to_add.copy())
+        return ret
+    
+    def create_solver(self, board: list[list[str]]) -> str:
+        # Create the moves
+        f = open('temp.txt', 'w')
+        for row in board:
+            for card in row:
+                f.write(card)
+                f.write(' ')
+            f.write('\n')
+        f.close()
+        f = open("moves.txt", "w")
+        subprocess.run(["./fc-solve.exe", "-m", "temp.txt"], stdout=f)
+        f.close()
+        subprocess.run(["rm", "temp.txt"])
+        # Read the moves into memory
+        f = open("moves.txt", "r")
+        data = f.read()
+        f.close()
+        subprocess.run(["rm", "moves.txt"])
+        return data
+    
+    def generate_moves(self, board: list[list[int]], data: str) -> Optional[list[list[tuple[str, int, str]], list[tuple[list[list[int]], list[int], list[int]]]]]:
+        'Dont know what it should return yet'
+        if 'I could not solve this game' in data:
+            return None
+        moves = list()
+        states = list()
+        initial_freecells = [255, 255, 255, 255]
+        initial_foundations = [255, 255, 255, 255]
+        initial_state = (tuple(tuple(col) for col in board), tuple(initial_freecells), tuple(initial_foundations))
+        states.append(initial_state)
+
+        curr_board = [col.copy() for col in board.copy()]
+        curr_cells = initial_freecells.copy()
+        curr_foundations = initial_foundations.copy()
+        # Loop through lines of data
+        for line in filter(lambda l: 'Move' in l, data.split('\n')):
+            move_src = ''
+            move_card = 1
+            move_dst = ''
+            words = line.split()
+            if words[1] != 'a': # Moving within columns
+                num_cards_to_move = int(words[1])
+                src = int(words[5])
+                dst = int(words[8])
+                cards_to_move = curr_board[src][-1*num_cards_to_move:].copy()
+                for card in cards_to_move:
+                    curr_board[dst].append(card)
+                for _ in range(num_cards_to_move):
+                    curr_board[src].pop()
+                move_src = 'C' + str(src)
+                move_card = num_cards_to_move
+                move_dst = 'C' + str(dst)
+            else:
+                src_type = words[4]
+                if src_type == 'stack':
+                    src = int(words[5])
+                    dst_type = words[7]
+                    move_src = 'C' + str(src)
+                    card = curr_board[src].pop()
+                    if dst_type == 'freecell':
+                        dst = int(words[8])
+                        curr_cells[dst] = card
+                        move_dst = 'F' + str(dst)
+                    elif dst_type == 'the': # foundations
+                        # Find the correct stack
+                        dst = -1
+                        for i in range(4):
+                            cf = curr_foundations[i]
+                            if (cf != 255) and ((cf%4) == (card%4)):
+                                dst = i
+                                break
+                        if dst == -1:
+                            dst = curr_foundations.index(255)
+                        curr_foundations[dst] = card
+                        move_dst = 'P' + str(dst)
+                    else:
+                        print('Issue reading data.')
+                        return None
+                elif src_type == 'freecell':
+                    src = int(words[5])
+                    dst_type = words[7]
+                    move_src = 'F' + str(src)
+                    card = curr_cells[src]
+                    curr_cells[src] = 255
+                    if dst_type == 'stack':
+                        dst = int(words[8])
+                        curr_board[dst].append(card)
+                        move_dst = 'C' + str(dst)
+                    elif dst_type == 'the':
+                        dst = -1
+                        for i in range(4):
+                            cf = curr_foundations[i]
+                            if (cf != 255) and ((cf%4) == (card%4)):
+                                dst = i
+                                break
+                        if dst == -1:
+                            dst = curr_foundations.index(255)
+                        curr_foundations[dst] = card
+                        move_dst = 'P' + str(dst)
+                    else:
+                        print('Issue reading data.')
+                        return None
+                else:
+                    print('Issue reading data.')
+                    return None
+            # Add the move and state
+            moves.append((move_src, move_card, move_dst))
+            state_to_add = (tuple(tuple(col) for col in curr_board), tuple(curr_cells), tuple(sorted(curr_foundations)))
+            states.append(state_to_add)
+        return [moves, states]
 
     def get_move(self) -> int:
         'Return the move index and the move.'
         # THERE IS NO LOOP IN HERE
         # run this every 0.1 seconds
         # Get the next move, by comparing the given game state and last move
+        mode = 'Manual'
         win = False
         board = rp.get_tableau()
         cells = rp.get_freecells()
-        foundations = sorted(rp.get_foundations())
-        if foundations == [48, 49, 50, 51]:
+        foundations = rp.get_foundations()
+        if not board or not cells or not foundations:
+            self.move_text['text'] = 'Could not read game data.'
+            return -1
+        elif sorted(foundations) == [48, 49, 50, 51]:
             self.move_text['text'] = 'You Win!'
             return 0
         else:
+            foundations = sorted(foundations)
             if self.lastMove == -1:
-                # Get initial state
-                # board = rp.get_tableau()
-                # cells = rp.get_freecells()
-                # foundations = sorted(rp.get_foundations())
                 state = (tuple(tuple(col) for col in board), tuple(cells), tuple(foundations))
                 self.curr_state = state
                 indices = [i for i, x in enumerate(self.states) if x == state]
                 for i in indices:
                     if i > self.lastMove:
+                        self.mode = 'Automatic'
                         self.lastMove = i
                         break
                 if self.lastMove == -1:
                     self.lastMove = 0
                 self.curr_move = self.moves[self.lastMove]
             else:
-                # board = rp.get_tableau()
-                # cells = rp.get_freecells()
-                # foundations = sorted(rp.get_foundations())
                 if foundations == [48, 49, 50, 51]:
                     win = True
                 state = (tuple(tuple(col) for col in board), tuple(cells), tuple(foundations))
@@ -67,7 +211,10 @@ class Solver:
                         if i > self.lastMove:
                             self.lastMove = i
                             found = True
+                            self.mode = 'Automatic'
                             break
+                    if not found:
+                        self.mode = 'Manual'
                     self.curr_state = state
                 self.curr_move = self.moves[self.lastMove]
             # Set the label
@@ -84,9 +231,9 @@ class Solver:
                 dst_type = 'the'
                 dst = 'foundations'
             if num_cards == 1:
-                text = f'Step {self.lastMove+1} out of {self.totalMoves}: Move {num_cards} card from {src_type} {src} to {dst_type} {dst}'
+                text = f'{self.mode} mode\nStep {self.lastMove+1} out of {self.totalMoves}: Move {num_cards} card from {src_type} {src} to {dst_type} {dst}'
             else:
-                text = f'Step {self.lastMove+1} out of {self.totalMoves}: Move {num_cards} cards from {src_type} {src} to {dst_type} {dst}'
+                text = f'{self.mode} mode\nStep {self.lastMove+1} out of {self.totalMoves}: Move {num_cards} cards from {src_type} {src} to {dst_type} {dst}'
             self.move_text['text'] = text
         self.root.after(100, self.get_move) # run itself again after 1000 ms
         return 0
@@ -105,9 +252,12 @@ class Solver:
         if cells != [255, 255, 255, 255] or foundations != [255, 255, 255, 255]:
             print('Freecells and/or foundations are corrupted. Make sure you have a new game ready and set.')
             return -1
-        new = erm.convertBoard(board) # new is [list[list[str]]]
-        data = erm.create_solver(new)
-        ret = erm.generate_moves(board, data)
+        # new = erm.convertBoard(board) # new is [list[list[str]]]
+        new = self.convertBoard(board)
+        # data = erm.create_solver(new)
+        data = self.create_solver(new)
+        # ret = erm.generate_moves(board, data)
+        ret = self.generate_moves(board, data)
         if not ret:
             print('Game is unsolveable.')
             return 1
@@ -138,9 +288,6 @@ class Solver:
 def main() -> int:
     s = Solver()
     status = s.loop()
-    # if status == -1:
-    #     return 1
-    # return 0
     return status == -1
 
 if __name__ == '__main__':

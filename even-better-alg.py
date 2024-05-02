dd266fa486ae31377acc86b2d92fa2e7b84d92a5
Danielle Croft <daniellec0321@gmail.com>
2024-05-01 20:17:37 -0400

    Can generate moves

--- even-better-alg.py
+++ even-better-alg.py
@@ -1,3 +1,5 @@
+import sys
+from typing import Optional
 from readProgram import readProgram
 
 class Move:
@@ -22,36 +24,29 @@ class Move:
                 return False
         return True
 
-    def generate_moves(self, board: list[list[int]], cells: list[int], foundations: list[int]) -> list[tuple[str, int, str]]:
+    def generate_moves(self, board: list[list[int]], cells: list[int], foundations: list[int]) -> Optional[list[tuple[str, int, str]]]:
         moves = list()
         # Column to column
         can_move = self.max_cards_to_move(board, cells)
         for idx, col in enumerate(board):
-            # print(f'Analyzing col {idx}...')
             for i in range(len(col)-1, -1, -1):
                 if len(col[i:]) > can_move:
                     break
                 if not self.cards_in_order(col[i:]):
                     break
-                # print(f'Checking this subcolumn: {col[i:]}')
                 for j, c in enumerate(board):
                     if j == idx:
                         continue
                     if not c and len(col[i:]) != can_move: # Empty column
-                        # print(f'Found empty column, appending {("C"+str(idx), len(col[i:]), "C"+str(j))}')
                         moves.append(("C"+str(idx), len(col[i:]), "C"+str(j)))
                         continue
                     if not c and len(col[i:]) == can_move:
-                        # print(f'Empty column, but not enough space to move card.')
                         continue
                     # Check if src card can go on this dst
                     if int(col[i]/4) != int(c[-1]/4)-1:
-                        # print(f'col[i] is {hex(col[i])}, c[-1] is {hex(c[-1])}. Wrong number')
                         continue
                     if self.get_color(col[i]) == self.get_color(c[-1]):
-                        # print(f'col[i] is {hex(col[i])}, c[-1] is {hex(c[-1])}. Wrong color')
                         continue
-                    # print(f'col[i] is {hex(col[i])}, c[-1] is {hex(c[-1])}. We\'re good! Appending move {("C"+str(idx), len(col[i:]), "C"+str(j))}')
                     moves.append(("C"+str(idx), len(col[i:]), "C"+str(j)))
         # Column to freecell
         freecell = -1
@@ -64,18 +59,45 @@ class Move:
                 if col:
                     moves.append(("C"+str(idx), 1, "F"+str(freecell)))
         # Column to foundations
+        for idx, col in enumerate(board):
+            if int(col[-1]/4) == 0: # There's an ace
+                freespace = foundations.index(255)
+                moves.append(("C"+str(idx), 1, "P"+str(freespace)))
+                continue
+            card_to_find = col[-1] - 4
+            if card_to_find in foundations:
+                dest = foundations.index(card_to_find)
+                moves.append(("C"+str(idx), 1, "P"+str(dest)))
         # Freecell to column
+        for idx, f in enumerate(cells):
+            for i, col in enumerate(board):
+                if int(f/4) != int(col[-1]/4)-1:
+                    continue
+                if self.get_color(f) == self.get_color(col[-1]):
+                    continue
+                moves.append(("F"+str(idx), 1, "C"+str(i)))
         # Freecell to foundations
+        for idx, f in enumerate(cells):
+            card_to_find = f - 4
+            if card_to_find in foundations:
+                dest = foundations.index(card_to_find)
+                moves.append(("F"+str(idx), 1, "P"+str(dest)))
         return moves
-
 
     def get_move(self, board: list[list[int]], cells: list[int], foundations: list[int]):
         pass
 
-if __name__ == '__main__':
+def main(args: list[str]) -> int:
     rp = readProgram()
     m = Move()
     board = rp.get_tableau()
     cells = rp.get_freecells()
     foundations = rp.get_foundations()
-    print(m.generate_moves(board, cells, foundations))
+    if not board or not cells or not foundations:
+        print(f'Error getting game data')
+        return 1
+    moves = m.generate_moves(board, cells, foundations)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
